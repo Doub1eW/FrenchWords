0e8387aa0779b4880c87dad6d36af1589b622f4e
William <william.wibeck@gmail.com>
2022-11-06 17:03:45 +0100

    test

---
+++ Words1.js
@@ -0,0 +1,9 @@
+Wordlist_Weekdays = [
+  { word: "Lundi", translation: "Måndag" },
+  { word: "Mardi", translation: "Tisdag" },
+  { word: "Mercredi", translation: "Onsdag" },
+  { word: "Jeudi", translation: "Torsdag" },
+  { word: "Vendredi", translation: "Fredag" },
+  { word: "Samedi", translation: "Lördag" },
+  { word: "Dimanche", translation: "Söndag" },
+];
