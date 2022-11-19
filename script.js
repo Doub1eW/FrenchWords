0d3549138bf8b9e03a18f4fc23d552b33cbc1958
William <96584578+Doub1eW@users.noreply.github.com>
2022-11-19 12:03:21 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -20,6 +20,10 @@ function resetPage() {
   totalStreak = 0;
 }
 
+function callNewCard() {
+  let obj = document.querySelector(".card");
+}
+
 function setWordlistChoiceTo1() {
   WordlistChoice = 1;
 
@@ -58,29 +62,29 @@ let inputValue;
 
 function chooseWeekdays() {
   wordlist = [
-    { word: "Måndag", translation: "lundi" },
-    { word: "Tisdag", translation: "mardi" },
-    { word: "Onsdag", translation: "mercredi" },
-    { word: "Torsdag", translation: "jeudi" },
-    { word: "Fredag", translation: "vendredi" },
-    { word: "Lördag", translation: "samedi" },
-    { word: "söndag", translation: "dimanche" },
+    { word: "Monday", translation: "lundi" },
+    { word: "Tuesday", translation: "mardi" },
+    { word: "Wednesday", translation: "mercredi" },
+    { word: "Thursday", translation: "jeudi" },
+    { word: "Friday", translation: "vendredi" },
+    { word: "Saturday", translation: "samedi" },
+    { word: "Sunday", translation: "dimanche" },
   ];
   return wordlist;
 }
 
 function chooseMonths() {
   wordlist = [
-    { word: "Januari", translation: "janvier" },
-    { word: "Februari", translation: "février" },
+    { word: "January", translation: "janvier" },
+    { word: "February", translation: "février" },
     { word: "Mars", translation: "mars" },
     { word: "April", translation: "avril" },
-    { word: "Maj", translation: "mai" },
-    { word: "Juni", translation: "juin" },
-    { word: "Juli", translation: "juillet" },
-    { word: "Augusti", translation: "août" },
+    { word: "May", translation: "mai" },
+    { word: "June", translation: "juin" },
+    { word: "July", translation: "juillet" },
+    { word: "August", translation: "août" },
     { word: "September", translation: "septembre" },
-    { word: "Oktober", translation: "octobre" },
+    { word: "October", translation: "octobre" },
     { word: "November", translation: "novembre" },
     { word: "December", translation: "décembre" },
   ];
@@ -89,19 +93,57 @@ function chooseMonths() {
 
 function chooseAnimals() {
   wordlist = [
-    { word: "Hund", translation: "chien" },
-    { word: "Katt", translation: "chat" },
-    { word: "Fisk", translation: "poisson" },
-    { word: "Fågel", translation: "oiseau" },
-    { word: "Kanin", translation: "lapin" },
-    { word: "Ko", translation: "vache" },
-    { word: "Häst", translation: "cheval" },
-    { word: "Höna", translation: "poule" },
-    { word: "Kyckling", translation: "poulet" },
+    { word: "Dog", translation: "chien" },
+    { word: "Cat", translation: "chat" },
+    { word: "Fish", translation: "poisson" },
+    { word: "Bird", translation: "oiseau" },
+    { word: "Rabbit", translation: "lapin" },
+    { word: "Cow", translation: "vache" },
+    { word: "Horse", translation: "cheval" },
+    { word: "Hen", translation: "poule" },
+    { word: "Chicken", translation: "poulet" },
+    { word: "Pig", translation: "cochon" },
+    { word: "Sheep", translation: "mouton" },
+    { word: "Goat", translation: "chèvre" },
+    { word: "Lion", translation: "lion" },
+    { word: "Tiger", translation: "tigre" },
+    { word: "Elephant", translation: "éléphant" },
+    { word: "Monkey", translation: "singe" },
+    { word: "Bear", translation: "ours" },
+    { word: "Deer", translation: "cerf" },
+    { word: "Wolf", translation: "loup" },
+    { word: "Fox", translation: "renard" },
+    { word: "Raccoon", translation: "raton laveur" },
+    { word: "Rat", translation: "rat" },
+    { word: "Mouse", translation: "souris" },
+    { word: "Snake", translation: "serpent" },
+    { word: "Frog", translation: "grenouille" },
+    { word: "Turtle", translation: "tortue" },
+    { word: "Duck", translation: "canard" },
+    { word: "Penguin", translation: "pingouin" },
+    { word: "Owl", translation: "chouette" },
+    { word: "Eagle", translation: "aigle" },
+    { word: "Hawk", translation: "faucon" },
+    { word: "Seagull", translation: "goéland" },
+    { word: "Dolphin", translation: "dauphin" },
+    { word: "Whale", translation: "baleine" },
+    { word: "Shark", translation: "requin" },
+    { word: "Octopus", translation: "pieuvre" },
+    { word: "Crab", translation: "crabe" },
+    { word: "Shrimp", translation: "crevette" },
+    { word: "Lobster", translation: "homard" },
+    { word: "Butterfly", translation: "papillon" },
+    { word: "Spider", translation: "araignée" },
+    { word: "Ant", translation: "fourmi" },
+    { word: "Bee", translation: "abeille" },
+    { word: "Ladybug", translation: "coccinelle" },
   ];
   return wordlist;
 }
 
+function chooseRandomWords() {
+  //wordlist =
+}
 const inputForm = document.querySelector("#inputForm");
 inputForm.addEventListener("submit", function (e) {
   e.preventDefault();
@@ -112,6 +154,13 @@ inputForm.addEventListener("submit", function (e) {
   checkTranslation(inputValue);
   rnWord = getRandomWord(chooseWordlist());
 });
+
+function toggleLightDarkmode() {
+  let body = document.body;
+  let topBar = document.querySelector(".topBar");
+  body.classList.toggle("darkmode");
+  topBar.classList.toggle("darkmode");
+}
 
 function moveMenuItem1() {
   document.querySelector(".menuitem1").style.transform = "translateY(-20%)";
@@ -141,43 +190,50 @@ function checkTranslation(inputValue) {
   if (inputValue == rnWord.translation) {
     var audio = new Audio("Assets/ding.mp3");
     audio.play();
-    (document.querySelector(".card").style.transform = "translateY(-50%)"),
+    (document.querySelector(".card").style.transform = "translateX(150%)"),
       (document.querySelector(".card").style.backgroundColor = "#00ff00"),
-      //(document.querySelector(".StreakCounter").style.backgroundColor =
-      // "#00ff00");
+      (document.querySelector(".wrongWordTranslation").style.color = "#111111"),
       setTimeout(function () {
-        (document.querySelector(".card").style.transform = "translateY(0)"),
-          (document.querySelector(".card").style.backgroundColor = "#6c11c8");
-        //(document.querySelector(".StreakCounter").style.backgroundColor =
-        //"#6c11c8");
+        //(document.querySelector(".card").style.transform = "translateY(0)"),
+        //document.querySelector(".card").style.backgroundColor = "#6c11c8";
       }, 300);
     AddOneToStreak();
   } else {
     var audio = new Audio("Assets/wrong.mp3");
     audio.play();
-    document.querySelector(".card").style.transform = "translateX(30%)";
+    //document.querySelector(".card").style.transform = "translateX(30%)";
+    document.querySelector(".card").style.transform = "translateX(-150%)";
+    document.querySelector(".wrongWordTranslation").innerHTML =
+      rnWord.translation;
 
     document.querySelector(".card").style.backgroundColor = "#FF0000";
     setTimeout(function () {
       document.querySelector(".card").style.color = "white";
-      document.querySelector(".card").style.transform = "translateX(-30%)";
-      document.querySelector(".card").style.backgroundColor = "#FF0000";
+      //document.querySelector(".card").style.transform = "translateX(-30%)";
+      //document.querySelector(".card").style.borderColor = "#FF0000";
+
+      document.querySelector(".wrongWordTranslation").style.color = "red";
     }, 150);
 
     setTimeout(function () {
-      document.querySelector(".card").style.transform = "translateX(30%)";
-      document.querySelector(".card").style.backgroundColor = "#FF0000";
+      //document.querySelector(".card").style.transform = "translateX(30%)";
+      //document.querySelector(".card").style.backgroundColor = "#FF0000";
     }, 300);
 
     setTimeout(function () {
-      document.querySelector(".card").style.transform = "translateX(-30%)";
-      document.querySelector(".card").style.backgroundColor = "#FF0000";
+      //document.querySelector(".card").style.transform = "translateX(-30%)";
+      //document.querySelector(".card").style.backgroundColor = "#FF0000";
     }, 450);
 
     setTimeout(function () {
-      document.querySelector(".card").style.transform = "translateX(0)";
-      document.querySelector(".card").style.backgroundColor = "#6c11c8";
+      //document.querySelector(".card").style.transform = "translateX(0)";
+      //document.querySelector(".card").style.backgroundColor = "#6c11c8";
+      document.querySelector(".card").style.borderColor = "#FFFFFF";
     }, 600);
+
+    setTimeout(function () {
+      document.querySelector(".wrongWordTranslation").innerHTML = "";
+    }, 3200);
     ResetStreak();
   }
   rnWord = getRandomWord(chooseWordlist());
